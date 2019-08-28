4b375a9a06d9d96d1349af32884bdecd79326d3c
Javier Reyes <javi_reyes1@hotmail.com>
2019-08-28 17:12:46 -0500

    Updates

---
+++ precache-manifest.5d11a83a24023612f566c0b624af4dd8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1451f92129b6d429cbb9a15c4f61abc",
+    "url": "/web-page/index.html"
+  },
+  {
+    "revision": "aca5af6f7f172324193a",
+    "url": "/web-page/static/css/2.c3517fa0.chunk.css"
+  },
+  {
+    "revision": "8fba70d8d7cdc942a387",
+    "url": "/web-page/static/css/main.a465e2fa.chunk.css"
+  },
+  {
+    "revision": "aca5af6f7f172324193a",
+    "url": "/web-page/static/js/2.90d384bf.chunk.js"
+  },
+  {
+    "revision": "8fba70d8d7cdc942a387",
+    "url": "/web-page/static/js/main.d48efce4.chunk.js"
+  },
+  {
+    "revision": "b1270a5a34bfb3b0f164",
+    "url": "/web-page/static/js/runtime~main.d74acbcd.js"
+  },
+  {
+    "revision": "5d418060b8732618156028c50dfa09d2",
+    "url": "/web-page/static/media/bio.5d418060.svg"
+  },
+  {
+    "revision": "22b038252b0f294da1fca25c00353be8",
+    "url": "/web-page/static/media/bioMaq.22b03825.mp4"
+  },
+  {
+    "revision": "05efb56dc7e1ad252b0c5117ea9d68f1",
+    "url": "/web-page/static/media/biobanner.05efb56d.svg"
+  },
+  {
+    "revision": "cc621285399ddd0bbb17d8eb636e04e9",
+    "url": "/web-page/static/media/bolsaspie.cc621285.svg"
+  },
+  {
+    "revision": "338f9e405847c13cdef1918785035ee2",
+    "url": "/web-page/static/media/contactenos.338f9e40.svg"
+  },
+  {
+    "revision": "be0f2f13bb12a2a8f39c24dad38d3c22",
+    "url": "/web-page/static/media/inicio.be0f2f13.svg"
+  },
+  {
+    "revision": "a20cfe6f5f3f2d491812c04161d40861",
+    "url": "/web-page/static/media/nosotros.a20cfe6f.svg"
+  },
+  {
+    "revision": "8c466bb24bab4b48b36ba417c8034be1",
+    "url": "/web-page/static/media/productos.8c466bb2.svg"
+  }
+]);
