7caa2f3a1d3400ec51fb0e01a14e140884fa3079
Javier Reyes <javi_reyes1@hotmail.com>
2019-12-13 09:36:07 -0500

    desing and email

--- server/index.js
+++ server/index.js
@@ -4,12 +4,8 @@ var express = require('express');
 var app = express();
 
 var auth = {
-  type: 'OAuth2',
   user: process.env.USUARIO,
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
-  refreshToken: process.env.refreshToken,
-  accessToken: process.env.accessToken
+  pass: process.env.PASSWORD
 };
 
 app.use(function(req, res, next) {
@@ -24,21 +20,15 @@ app.post('/api/send', function(req, res) {
   console.log(req.query.name);
   console.log(req.query.mail);
   console.log(req.query.messaje);
-  // eslint-disable-next-line no-undef
-  // response = {
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   message: req.body.message
-  // }
 
   var mailOptions = {
       from: req.query.mail,
       to: req.query.mail,
       subject: 'Solicitud de Información de ' + req.query.name,
-      html: 'Mensaje' + req.query.messaje,
+      html: 'Mensaje :' + req.query.messaje,
   };
 var transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: 'mail.aagbolsasplasticas.com.co',
       port: 465,
       secure: true,
       auth: auth
@@ -51,14 +41,6 @@ transporter.sendMail(mailOptions, (err, res) => {
       }
   });
 })
-
-
-// app.get('/api/greeting', (req, res) => {
-//   const name = req.query.name || 'World';
-//   console.log("Javier Reyes");
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-// });
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
